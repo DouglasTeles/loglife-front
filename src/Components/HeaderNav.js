9a6991e60235bacb8018afd90598465d1d4be7fb
DouglasTeles <douglastelesoliveira@gmail.com>
2021-07-20 02:40:17 -0300

    update

--- src/Components/HeaderNav.js
+++ src/Components/HeaderNav.js
@@ -1,19 +1,37 @@
+import { Dropdown, Button } from 'react-bootstrap'
 import React from 'react'
-
+import Menu from '../assets/drop.png'
 import Logo from '../assets/logo_loglife.png'
+
 
 function HeaderNav() {
     return (
         <header>
             <nav>
-                <a href="/"><img alt="Logo" src={Logo}/></a>
-                
+                <a href="/"><img alt="Logo" src={Logo} /></a>
+
                 <div>
-                <a href="/">Início</a>
-                <a href="/cadastro">Cadastro</a>
-                <a href="/visualizar">Visualizar Clientes</a>
+                    <a className="a" href="/">Início</a>
+                    <a className="a"  href="/cadastro">Cadastro</a>
+                    <a className="a"  href="/visualizar">Visualizar Clientes</a>
                 </div>
+                
             </nav>
+           
+            <Dropdown className="drop">
+                <Dropdown.Toggle variant="primary" id="dropdown-basic">
+                <a><img alt="Menu" src={Menu}/></a>
+                </Dropdown.Toggle>
+
+                <Dropdown.Menu>
+                    <Dropdown.Item href="/">Início</Dropdown.Item>
+                    <Dropdown.Item href="/cadastro">Cadastro</Dropdown.Item>
+                    <Dropdown.Item href="/visualizar">Visualizar Clientes</Dropdown.Item>
+                </Dropdown.Menu>
+            </Dropdown>
+
+            
+
         </header>
     )
 }
